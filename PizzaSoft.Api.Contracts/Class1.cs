7e971af64cd32878d368b9c4fa28d23acc6149a9
Luz Soto <xqluxqaqua@gmail.com>
2022-01-29 23:05:24 -0400

    INITIAL COMMIT

---
+++ PizzaSoft.Api.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaSoft.Api.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ PizzaSoft.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaSoft.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
