07f226e783c12038a5bc4d7ffcd6ebec8d6bbd18
Luz Soto <xqluxqaqua@gmail.com>
2022-01-29 23:54:25 -0400

    ENTITIES CREATED

---
+++ PizzaSoft.Domain/Common/BaseResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaSoft.Domain.Common
+{
+    public class BaseResponse<TData>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string ErrorCode { get; set; }
+        public TData Data { get; set; }
+        public List<ErrorModel> Errors { get; set; } = new List<ErrorModel>();
+
+        public BaseResponse(string code, string message, List<ErrorModel> errorModels)
+        {
+            this.Success = false;
+            this.ErrorCode = code;
+            this.Message = message;
+            this.Errors = errorModels;
+        }
+
+        public BaseResponse(string code, string message)
+        {
+            this.Success = false;
+            this.Message = message;
+            this.ErrorCode = code;
+        }
+
+        public BaseResponse(TData data)
+        {
+            this.Success = true;
+            this.Message = ErrorMessages.SUC000;
+            this.Data = data;
+        }
+    }
+
+    public class ErrorModel
+    {
+        public string FieldName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ PizzaSoft.Domain/Entities/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaSoft.Domain.Entities
+{
+    public class Pizza
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public double Price { get; set; }
+        public Guid[] Toppings { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
